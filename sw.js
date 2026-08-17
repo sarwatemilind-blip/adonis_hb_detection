1122364e30aa40fef88911f64e773a63115b8541
sarwatemilind-blip <sarwatemilind@gmail.com>
2026-08-17 23:42:25 +0530

    Fix: Add AI model files and bump SW cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='adonis-hb-v2-1';
-const ASSETS=['./','./index.html','./manifest.json','./assets/MV30.png','./assets/AMINO-GLOBIN-XT.png'];
+const CACHE='adonis-hb-v2-2';
+const ASSETS=['./','./index.html','./manifest.json','./assets/MV30.png','./assets/AMINO-GLOBIN-XT.png','./model.onnx','./model.onnx.data'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
