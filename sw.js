e9c97360383bd893f088999d9d594aa322bc188b
sarwatemilind-blip <sarwatemilind@gmail.com>
2026-08-19 08:40:08 +0530

    Feat: Layer 1 manual crop contamination gate & UI cleanups

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='adonis-hb-v2-14';
+const CACHE='adonis-hb-v2-15';
 const ASSETS=['./','./index.html','./manifest.json','./assets/MV30.png','./assets/AMINO-GLOBIN-XT.png','./model.onnx','./model.onnx.data','https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.css','https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.js','./feature_extractor.js','./eye_app_training_v1/hgb_regressor_image_only.onnx'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
